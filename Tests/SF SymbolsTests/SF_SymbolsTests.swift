7d2a313cff58e4b23dfbb71243cc1ab772111ccc
OrigamiKing3612 <origamiking3612@gmail.com>
2023-12-31 14:07:31 -0600

    Initial Commit

---
+++ Tests/SF SymbolsTests/SF_SymbolsTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import SF_Symbols
+
+final class SF_SymbolsTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
